b33ec4743f14d9a711a4d010c74974528153c5b7
Oleksandr <107481048+RaccoonBLR@users.noreply.github.com>
2023-05-11 01:02:13 +0300

    add actions

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+} = require("./contacts");
+const { program } = require("commander");
 
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await listContacts();
+      console.table(contacts);
+      break;
+
+    case "get":
+      const contact = await getContactById(id);
+      console.log(contact);
+      break;
+
+    case "add":
+      const newContact = await addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const remoteContact = await removeContact(id);
+      console.log(remoteContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+program
+  .option("-a, --action, <type>")
+  .option("-i, --id, <type>")
+  .option("-n, --name, <type>")
+  .option("-e, --email, <type>")
+  .option("-p, --phone, <type>");
+
+program.parse();
+
+const options = program.opts();
+invokeAction(options);
